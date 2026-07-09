8bde2292735be5f6e6f34f6a16826eb3cb0f7d33
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-07-09 20:47:06 +0530

    feat: integrate right card into section layout

--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -1,7 +1,10 @@
+import RightCard from "./RightCard"
 
 const RightContent = () => {
   return (
-    <div className="h-full w-2/3 bg-blue-200"></div>
+    <div className="h-full p-6 w-2/3">
+      <RightCard/>
+    </div>
   )
 }
 
