71c86579adf30da16a2b2bba32c29f58b47f89ea
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-07-10 21:48:03 +0530

    feat: refine hero section typography and content

--- src/components/Section1/HeroText.jsx
+++ src/components/Section1/HeroText.jsx
@@ -3,7 +3,7 @@ const HeroText = () => {
     <div className='p-6'>
             <h3 className=' mb-7 text-6xl font-bold'> Prospective<br/> 
             <span>customer</span> <br/>Segmentation</h3>
-            <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto libero illum deserunt repellat eveniet eius debitis ex quis quisquam ut.</p>
+            <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto libero illum deserunt repellat eveniet eius debitis ex quis quisquam ut.</p>
     </div>
   )
 }
