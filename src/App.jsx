7c413e569c2aa0cb502a182d2a8fc648a88b7e5f
ShubhamNegi-07 <shubhamsingh82735@gmail.com>
2026-07-10 21:49:27 +0530

    feat: integrate second section into application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import Section1 from './components/Section1/Section1'
 import Section2 from './components/Section2/Section2'
+import workImg1 from "./assets/Work img1.jpg";
+import workImg2 from "./assets/Work img2.jpg";
+import workImg3 from "./assets/Work img3.jpg";
+import workImg4 from "./assets/Work img4.jpg";
+import workImg5 from "./assets/Work img5.jpg";
 
 const App = () => {
+
+  const users = [
+    {
+      img: workImg1,
+      intro: '',
+      tag: 'Satisfied'
+    },
+    {
+      img: workImg2,
+      intro: '',
+      tag:  'Underserved'
+    },
+   {
+      img: workImg3,
+      intro: '',
+      tag: 'Underbanked'
+    },
+    {
+      img: workImg4,
+      intro: '',
+      tag: 'Aesthetic'
+    },
+    {
+      img: workImg5,
+      intro: '',
+      tag: 'professional'
+    }
+  ]
+
   return (
     <div>
-      <Section1/>
+      <Section1 users = {users}/>
       <Section2/>
     </div>
   )
